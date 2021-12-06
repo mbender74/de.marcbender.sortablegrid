6d6e5c5e2728ceafd30d0619b21fe5cac4452305
Marc Bender <marc_bender@icloud.com>
2021-12-06 11:55:32 +0100

    updated demo app

--- example/testapp/Resources/app.js
+++ example/testapp/Resources/app.js
@@ -1,7 +1,169 @@
 var cirularProgressModule = require("ti.circularprogress");
  var SortableView = require("/lib/SortableView");
+ var fontawesome = require('/lib/IconicFont').IconicFont({font: '/lib/FontAwesome',ligature: false});
 
-var sortableGridModule = require("de.marcbender.sortablegrid");
+ var sortableGridModule = require("de.marcbender.sortablegrid");
+
+var sliderModule = require("de.marcelpociot.circularslider");
+var gridView;
+var tr_init = Titanium.UI.createMatrix2D();
+var tr_second = Titanium.UI.createMatrix2D();
+var tr_third = Titanium.UI.createMatrix2D();
+
+var tr_start = tr_init.rotate(-1,0);
+var tr_anim = tr_second.rotate(2,0);
+var tr_end = tr_third.rotate(-2,0);
+var wobbeling = false;
+
+var gridCells = [];
+
+
+function createRemoveButton(parent){
+		
+
+	var removeButtonContainer = viewFunc.createView({
+		parentCell:parent,
+		visible:true,
+		height:Ti.UI.SIZE,
+		width:Ti.UI.SIZE,
+		top:0,
+		right:0,
+		zIndex:200
+	});
+
+	var removeImageButtonContainer = viewFunc.createView({
+		backgroundColor:'#d0d0d0',
+		
+		touchEnabled:false,
+		bubbleParent:true
+	});
+
+
+	var removeImageButton = Ti.UI.createImageView({
+		width: Ti.UI.SIZE,
+		height: 34,
+		touchEnabled:false,
+		bubbleParent:true
+	});
+	Ti.UI.createLabel({
+		width: Ti.UI.SIZE,
+		height: Ti.UI.SIZE,
+		color : 'rgba(63, 69, 81, 0.8)',
+		textAlign:'center',
+		font: {
+			fontSize: 26,
+			fontFamily: fontawesome.fontfamily()
+		},
+		text: fontawesome.icon('fa-minus-circle')
+	}).toImage(function(e){
+		var newDim = Math.ceil(e.width/2);
+		removeImageButton.image = e;
+		removeImageButtonContainer.applyProperties({
+			width:34,
+			height:34,
+			borderRadius:17
+			//borderRadius:Math.ceil(newDim/2)	
+		})
+	},true);
+
+
+	
+	removeImageButtonContainer.add(removeImageButton);
+	removeButtonContainer.add(removeImageButtonContainer);
+	
+
+	removeButtonContainer.addEventListener('touchstart', function(e){
+		removeImageButton.opacity = 0.8;
+		removeImageButton.tintColor = 'red';
+	});
+
+	removeButtonContainer.addEventListener('touchend', function(e){
+		removeImageButton.opacity = 1.0;		
+		removeImageButton.tintColor = null;	
+	});
+
+	removeButtonContainer.addEventListener('touchcancel', function(e){
+		removeImageButton.opacity = 1.0;		
+		removeImageButton.tintColor = null;	
+	});
+
+
+
+	return removeButtonContainer;
+}
+
+
+
+function shake(view) {
+	view.firstRun = true;
+
+	//Translation to start from
+	view.transform = tr_start;
+	
+	//Animation
+	var a = Titanium.UI.createAnimation();
+	a.curve = Titanium.UI.ANIMATION_CURVE_EASE_IN_OUT;
+	a.transform = tr_anim;
+	a.duration = 125;
+	a.autoreverse = true;
+	a.repeat = 2000;
+	a.delay = 0;
+
+	//Return to initial position
+	a.addEventListener('complete',function() {
+		if (completeCounter == cells.length-1){
+			editMode(false);
+			completeCounter = 0;
+		}
+		else {
+			completeCounter++;
+		}
+	});
+	view.wobbleAnimation = a;
+	//Execute Animation
+	view.animate(a);
+  }
+
+ function wobble(){
+	for (var i = 0; i < gridCells.length; i++) {
+		shake(gridCells[i]);
+		// cells[i].draggable.setConfig({
+		// 	enabled : true
+		// });
+	}
+ }
+  
+ var editMode = function(status) {
+	if (status == false){
+		var restTR = Titanium.UI.createMatrix2D();
+		restTR.rotate(-2,0);
+
+		for (var i = 0; i < gridCells.length; i++) {
+			//cells[i].removeButton.hide();
+
+			// if (!isAndroid){
+			// 	cells[i].cellContent.stopAnimations();
+			// 	cells[i].cellContent.transform = restTR;
+			// }
+			// else {
+				gridCells[i].stopAnimation();
+			//}
+
+			// cells[i].draggable.setConfig({
+			// 	enabled : true
+			// });	
+		}
+		wobbeling = false;
+	}
+	
+ }
+
+
+
+
+
+
+
 
 
 function getRandomColor() {
@@ -108,9 +270,10 @@ function createGridDashBoardViews(size){
 	var sortableViewData = [];
 	for (var i = 0; i < size; i++){
 
-	   var v = Ti.UI.createView({
-		  height:Ti.UI.FILL,
-		  width:Ti.UI.FILL,
+	   var v = viewFunc.createView({
+		  id:i,
+		  height:110,
+		  width:110,
 		  borderRadius: 14,
 		  borderWith:1,
 		  borderColor:'#ccffffff',
@@ -134,6 +297,22 @@ function createGridDashBoardViews(size){
 			  y: 1
 		  }
 		}));
+		// v.addEventListener('longpress', function(e){
+		// 	if (wobbeling == false){
+		// 		wobbeling = true;
+		// 		wobble();
+		// 	}
+		// 	else {
+		// 		editMode(false);
+		// 	}
+		// });
+
+		var removeButton = createRemoveButton(v);
+
+		removeButton.addEventListener("click",function(e){
+			gridView.deleteItem(this.parentCell.id);
+		});
+		v.add(removeButton);
 
 		sortableViewData.push(v);	
 	}
@@ -217,17 +396,36 @@ function createDashBoardButtons(size){
 	//clipMode:Ti.UI.iOS.CLIP_MODE_DISABLED
 });
 
+gridCells = createGridDashBoardViews(10);
 
-var gridView = sortableGridModule.createView({
+gridView = sortableGridModule.createView({
 	height:Ti.UI.FILL,
 	width:Ti.UI.FILL,
-	top:20,
-	backgroundColor:'red',
-	items:createGridDashBoardViews(10)
+	top:30,
+	bottom:30,
+	left:0,
+	right:0,
+	backgroundColor:'transparent',
+	data:gridCells
 })
 
 
 win.add(gridView);
+
+
+
+// var sliderView = sliderModule.createView({
+// 	height: 250,
+// 	width: 250,
+// 	backgroundColor:'red',
+// 	lineWidth: 24,
+// 	filledColor: 'blue',
+// 	unfilledColor: 'grey'
+// });
+// sliderView.addEventListener('change',function(e){
+// 	Ti.API.info( "Value is: ", e.value );
+// });
+//win.add( sliderView );
 
 
 win.open();
